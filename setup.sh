4c16ede168a5921c5dc30f51d81fe4b425708f87
Edward Williamson <edward.l.williamson@boeing.com>
2023-01-09 08:40:41 -0600

    First pass at setup script, needs work

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Script to setup .vimrc and .vim folder from git repo
+
+ln -s ~/myvim/vimrc ~/.vimrc
+mkdir ~/.vim
+ln -s ~/myvim/colors ~/.vim/colors
+
+ln -s ~/myvim/mappings.vim ~/.vim/mappings.vim
+ln -s ~/myvim/mycolorscheme.vim ~/.vim/mycolorscheme.vim
+
